5cdaa3a6290f438d8229a7fc819ae026ac97f952
24bisquitz <62778411+24bisquitz@users.noreply.github.com>
2020-04-03 18:20:11 +0200

    Add files via upload

---
+++ Rotation/rotLand.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 
+#rotate screen and touchscreen to LANDSCAPE mode
+
+xrandr -o right
+xinput set-prop "ELAN1001:00 04F3:202C" --type=float "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
+
+#xrandr -o normal
+#xinput set-prop "ELAN1001:00 04F3:202C" --type=float "Coordinate Transformation Matrix" 0 0 0 0 0 0 0 0 0
