fa843545ef10657509ef8cc54687e26c48f1e6db
24bisquitz <62778411+24bisquitz@users.noreply.github.com>
2020-04-07 16:21:58 +0200

    Update rotLand.sh

--- Rotation/rotLand.sh
+++ Rotation/rotLand.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # 
-#rotate screen and touchscreen to LANDSCAPE mode
-
+# rotate display to LANDSCAPE mode
 xrandr -o right
+# rotate touchscreen to LANDSCAPE mode
 xinput set-prop "ELAN1001:00 04F3:202C" --type=float "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
-
-#xrandr -o normal
-#xinput set-prop "ELAN1001:00 04F3:202C" --type=float "Coordinate Transformation Matrix" 0 0 0 0 0 0 0 0 0
