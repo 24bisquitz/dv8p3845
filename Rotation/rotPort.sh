4288912dcb523f79f6224c731b5f130708e09d75
24bisquitz <62778411+24bisquitz@users.noreply.github.com>
2020-04-07 16:23:01 +0200

    Update rotPort.sh

--- Rotation/rotPort.sh
+++ Rotation/rotPort.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 # 
-#rotate screen and touchscreen to portrait mode
-
-#xrandr -o right
-#xinput set-prop "ELAN1001:00 04F3:202C" --type=float "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
-
+# rotate display to PORTRAIT mode
 xrandr -o normal
+# rotate touchscreen to portrait mode
 xinput set-prop "ELAN1001:00 04F3:202C" --type=float "Coordinate Transformation Matrix" 0 0 0 0 0 0 0 0 0
